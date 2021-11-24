697335dc77a479cc2e85a4f50c67d783e6f2016f
bcharlotin1 <biancacharlotin1@gmail.com>
2021-11-24 16:30:36 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -4,6 +4,7 @@ class Timer extends Component {
   constructor() {
     super();
     this.timer = React.createRef();
+    // step 1 to change clolor
     this.state = {
       time: 0,
       color: "#" + Math.floor(Math.random() * 16777215).toString(16)
@@ -18,6 +19,18 @@ class Timer extends Component {
       this.props.updateInterval * 1000
     );
   }
+  componentDidUpdate(){
+    // step 3 to change clolor
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -26,7 +39,7 @@ class Timer extends Component {
   render() {
     const { time, color, logText } = this.state;
     return (
-      <section className="Timer" style={{ background: color }} ref={this.timer}>
+      <section className="Timer" style={{ background: color }} ref={this.timer}> 
         <h1>{time}</h1>
         <button onClick={this.stopClock}>Stop</button>
         <aside className="logText">{logText}</aside>
